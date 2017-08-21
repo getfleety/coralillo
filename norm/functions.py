b9bb147e2917d8626ac43eaedc60a2aff5fac94c
Abraham Toriz <awonderfulcode@gmail.com>
2017-08-21 14:46:59 -0500

    include lua directory in packages

--- norm/functions.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-from random import random
-
-def current_timestamp():
-    return datetime.now().replace(microsecond=0)
-
-def random_location():
-    return [random()*160-80, random()*360-180]
