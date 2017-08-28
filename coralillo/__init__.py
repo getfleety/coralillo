d6499c61ff60bd75f4f6e18ac4559ed2ce7cce85
Abraham Toriz <awonderfulcode@gmail.com>
2017-08-28 14:40:37 -0500

    be able to change how ids are generated

--- coralillo/__init__.py
+++ coralillo/__init__.py
@@ -1,16 +1,23 @@
 import redis
 from coralillo.lua import Lua
+from uuid import uuid1
+
+
+def uuid1_id():
+    return uuid1().hex
 
 
 class Engine:
 
-    def __init__(self, **kwargs):
+    def __init__(self, id_function=uuid1_id, **kwargs):
         try:
             url = kwargs.pop('url')
 
             self.redis = redis.StrictRedis.from_url(url)
         except:
             self.redis = redis.StrictRedis(**kwargs)
+
+        self.id_function = id_function
 
         self.lua = Lua(self.redis)
 
