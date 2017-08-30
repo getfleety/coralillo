0937c2e814eb3c708fc0eff45f58f88f1705fa14
Abraham Toriz <awonderfulcode@gmail.com>
2017-08-29 23:36:36 -0500

    document helpers close #14

--- coralillo/utils.py
+++ coralillo/utils.py
@@ -2,6 +2,9 @@ from redis.client import BasePipeline
 import re
 
 def to_pipeline(redis):
+    ''' If the argument is a redis connection this function makes a redis
+    pipeline from it. Otherwise it returns the object passed, so it ensures
+    it is a pipeline '''
     if isinstance(redis, BasePipeline):
         return redis
 
@@ -9,9 +12,14 @@ def to_pipeline(redis):
 
 # from https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
 def snake_case(string):
+    ''' Takes a string that represents for example a class name and returns
+    the snake case version of it. It is used for model-to-key conversion '''
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
 
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 def camelCase(string):
+    ''' Takes a string that represents the redis key version of a model name
+    and returns its camel case version. It is used for key-to-model
+    conversion. '''
     return ''.join(s[0].upper()+s[1:] for s in string.split('_'))
