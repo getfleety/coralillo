ee37a08b6bcec898590f2448b037d2985820dc0e
Abraham Toriz <awonderfulcode@gmail.com>
2017-10-06 12:33:33 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='coralillo',
 
-    version='0.6.3',
+    version='0.6.4',
 
     description='An Object-Redis Mapping',
     long_description="""Coralillo provides a familiar API to store your objects
